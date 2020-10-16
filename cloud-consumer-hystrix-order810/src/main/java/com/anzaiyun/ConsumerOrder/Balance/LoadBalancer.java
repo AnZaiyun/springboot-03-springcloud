85b7f4e37997149a6e280a11f5e146268f63eec5
AnZaiyun <1732478192@qq.com>
2020-10-16 15:53:01 +0800

    [更新项目]删除服务降级模块多余文件

--- cloud-consumer-hystrix-order810/src/main/java/com/anzaiyun/ConsumerOrder/Balance/LoadBalancer.java
+++
@@ -1,9 +0,0 @@
-package com.anzaiyun.ConsumerOrder.Balance;
-
-import org.springframework.cloud.client.ServiceInstance;
-
-import java.util.List;
-
-public interface LoadBalancer {
-    ServiceInstance instances(List<ServiceInstance> serviceInstances);
-}
--- cloud-consumer-hystrix-order810/src/main/java/com/anzaiyun/ConsumerOrder/Controller/TestController.java
+++ cloud-consumer-hystrix-order810/src/main/java/com/anzaiyun/ConsumerOrder/Controller/TestController.java
@@ -1,18 +1,11 @@
 package com.anzaiyun.ConsumerOrder.Controller;
 
-import com.anzaiyun.ConsumerOrder.Balance.LoadBalancer;
 import com.anzaiyun.ConsumerOrder.FeginService.PaymentFeginService;
-import com.anzaiyun.entity.CommonResult;
 import org.apache.log4j.Logger;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
 import javax.annotation.Resource;
-import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/test")
@@ -20,15 +13,8 @@ public class TestController {
 
     private Logger logger = Logger.getLogger(TestController.class);
 
-//    public static final String PAYMENT_URL = "http://localhost:8001";
-//    改为微服务名称
-//    public static final String PAYMENT_URL = "http://cloud-provider-hystrix-payment";
     @Resource
     private PaymentFeginService paymentFeginService;
-
-    //自定义的负载均衡
-//    @Resource
-//    private LoadBalancer loadBalancer;
 
     //对于注册进eureka的微服务，可以通过服务发现的方式来获取服务信息
     @Resource
