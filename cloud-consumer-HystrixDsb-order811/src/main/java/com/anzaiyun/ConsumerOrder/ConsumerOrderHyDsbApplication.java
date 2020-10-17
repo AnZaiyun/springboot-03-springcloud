ca4f8047985e7d5ffc76de9811d51b21d25ec004
AnZaiyun <1732478192@qq.com>
2020-10-17 15:53:35 +0800

    [更新项目]Hystrix服务监控模块

--- cloud-consumer-HystrixDsb-order811/src/main/java/com/anzaiyun/ConsumerOrder/ConsumerOrderHyDsbApplication.java
+++ cloud-consumer-HystrixDsb-order811/src/main/java/com/anzaiyun/ConsumerOrder/ConsumerOrderHyDsbApplication.java
@@ -2,16 +2,10 @@ package com.anzaiyun.ConsumerOrder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableDiscoveryClient
-@EnableFeignClients
-@EnableHystrix
+@EnableHystrixDashboard
 public class ConsumerOrderHyDsbApplication {
 
     public static void main(String[] args) {
--- cloud-provider-hystrix-payment8010/src/main/java/com/anzaiyun/ProviderPayment/ProviderPaymentHY8010Application.java
+++ cloud-provider-hystrix-payment8010/src/main/java/com/anzaiyun/ProviderPayment/ProviderPaymentHY8010Application.java
@@ -1,10 +1,13 @@
 package com.anzaiyun.ProviderPayment;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,5 +17,22 @@ public class ProviderPaymentHY8010Application {
 
     public static void main(String[] args) {
         SpringApplication.run(ProviderPaymentHY8010Application.class,args);
+    }
+
+    /**
+     * 这一段没明白是什么意思？？？
+     * 此配置是为了监控服务而配置，与服务容错本身无关
+     * ServletRegistrationBean因为springboot的默认路径不是“hystrix.stream”
+     * 所以需要在自己的项目里配置上HystrixMetricsStreamServlet
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
